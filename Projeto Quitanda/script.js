aef1448a5c452f27ef79a12a1eed96a61620b205
Any Gabriely <129224785+any-g@users.noreply.github.com>
2024-09-01 20:51:17 -0300

    Update script.js

--- Projeto Quitanda/script.js
+++ Projeto Quitanda/script.js
@@ -2,5 +2,5 @@ let carrinho = []
 function adicionarCarrinho(){
     let frutas = document.getElementById("frutas")
     let quantidade = document.getElementById("quantidade")
-    
+    carrinho.push("frutas")
 }

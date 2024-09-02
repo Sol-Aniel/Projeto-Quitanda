52d6c97b31fdd034ba5d110bcbc43f17e9735b1a
Any Gabriely <129224785+any-g@users.noreply.github.com>
2024-09-02 14:02:27 -0300

    Update script.js

--- Projeto Quitanda/script.js
+++ Projeto Quitanda/script.js
@@ -1,6 +1,31 @@
 let carrinho = []
+frutas{
+    nome:nome
+    preco:preco
+    quantidade:quantidade
+}
+
 function adicionarCarrinho(){
+    
+    for frutas in carrinho{
     let frutas = document.getElementById("frutas")
+      let frutas.nome("frutas")
     let quantidade = document.getElementById("quantidade")
+     let frutas.quantidade(quantidade)
+    if frutas==laranja
+         frutas.preco=1.50
+    else if frutas==maça
+         frutas.preco=3.00
+    else if frutas==banana
+         frutas.preco=2.00
+    else 
+         frutas.preco=6.00
     carrinho.push("frutas")
 }
+}
+function resultFinal(frutas){
+      frutas.preco=frutas.preco*frutas*quantidade
+      console.log("frutas")
+}
+
+
